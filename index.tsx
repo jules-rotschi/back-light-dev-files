45648a34049fafd136088762b4f976f0972b74f5
Jules Rotschi <rotschi.jules@gmail.com>
2024-05-29 18:03:36 +0200

    Refactor components style

--- index.tsx
+++ index.tsx
@@ -1,12 +1,28 @@
 import { createRoot } from "react-dom/client";
-import { Checkbox } from "./components/Checkbox";
+import { Snackbar } from "./components/Snackbar";
+import { Select } from "./components/Select";
+import { Button } from "./components/Button";
+import { SmallButton } from "./components/SmallButton";
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 const App = () => {
 
+  const options = [
+    {
+      label: "Option 1",
+      value: "1"
+    },
+    {
+      label: "Option 2",
+      value: "2"
+    }
+  ]
+
   return (
-    <Checkbox id="check" label="Test" />
+    <>
+    <SmallButton style="outlined" color="success" label="Test" />
+    </>
   )
 }
 
